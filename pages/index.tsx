c484812fd872ef60b62a7dba7e55de25f1af1101
kubotama <16697573+kubotama@users.noreply.github.com>
2021-07-28 14:53:22 +0900

    refactor: タイマーの状態をstateに保持する
    stateの値によって、停止あるいは開始をボタンのラベルとして表示する

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,64 +1,78 @@
+import { useState } from 'react'
+
 import Head from 'next/head'
 
-export const Home = (): JSX.Element => (
-  <div className="container">
-    <Head>
-      <title>Pomodoro Timer</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+interface TimerState {
+  isTimerOn: boolean
+}
 
-    <main>
-      <h1 className="title">ポモドーロ・タイマー</h1>
-      <div className="time" data-testid="timeLeft">
-        --:--
-      </div>
-      <button data-testid="timerButton">開始</button>
-    </main>
+export const Home = (): JSX.Element => {
+  const [state, setState] = useState<TimerState>({
+    isTimerOn: false,
+  })
+  return (
+    <div className="container">
+      <Head>
+        <title>Pomodoro Timer</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
+      <main>
+        <h1 className="title">ポモドーロ・タイマー</h1>
+        <div className="time" data-testid="timeLeft">
+          --:--
+        </div>
+        <button data-testid="timerButton">
+          {state.isTimerOn ? '停止' : '開始'}
+        </button>
+      </main>
 
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
 
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
 
-      .time {
-        font-size: 2rem;
-      }
-    `}</style>
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
 
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
+        .time {
+          font-size: 2rem;
+        }
+      `}</style>
 
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-)
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 export default Home
